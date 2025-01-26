668e45a1d97d8ed670635d68d30a61e1107abd67
ehuang07 <huangsimiao2014@gmail.com>
2025-01-25 19:36:34 -0800

    more init

--- backend2/src/db/mongoConnection.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DATABASE_URL as string);
-  } catch (error) {
-    throw new Error('MongoDB connection failed');
-  }
-};
--- backend2/src/errors/ErrorWithCode.ts
+++
@@ -1,4 +0,0 @@
-export default interface ErrorWithCode extends Error {
-  code?: number;
-}
-
